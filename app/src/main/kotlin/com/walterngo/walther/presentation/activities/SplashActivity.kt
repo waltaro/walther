08b66d265b50f50f6134d03ed7238c933415e1cf
Walter Ngo <ngo.walter@gmail.com>
2017-04-06 11:43:57 +1000

    Convert SplashActivity to Kotlin, add Splash theme, add Anko

---
+++ app/src/main/kotlin/com/walterngo/walther/presentation/activities/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.walterngo.walther.presentation.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.walterngo.walther.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
