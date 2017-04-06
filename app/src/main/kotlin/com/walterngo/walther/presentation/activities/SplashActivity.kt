9c10729da0af318b9cdba25cc8bc12c7d2d06086
Walter Ngo <ngo.walter@gmail.com>
2017-04-06 11:52:31 +1000

    Add Anko, add SplashActivity

--- app/src/main/kotlin/com/walterngo/walther/presentation/activities/SplashActivity.kt
+++ app/src/main/kotlin/com/walterngo/walther/presentation/activities/SplashActivity.kt
@@ -1,14 +1,21 @@
 package com.walterngo.walther.presentation.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.support.v7.app.AppCompatActivity
 import com.walterngo.walther.R
+import com.walterngo.walther.presentation.presenters.SplashPresenter
+import org.jetbrains.anko.startActivity
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), SplashPresenter.ViewSurface {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        navigateToMain()
+    }
+
+    override fun navigateToMain() {
+        startActivity<MainActivity>()
+        finish()
     }
 }
--- app/src/main/kotlin/com/walterngo/walther/presentation/presenters/SplashPresenter.kt
+++ app/src/main/kotlin/com/walterngo/walther/presentation/presenters/SplashPresenter.kt
@@ -4,4 +4,10 @@ package com.walterngo.walther.presentation.presenters
  * @author walterngo
  */
 class SplashPresenter {
+
+
+    interface ViewSurface {
+        fun navigateToMain();
+    }
+
 }
